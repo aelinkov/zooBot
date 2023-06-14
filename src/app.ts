ddda52626e5b57e0f881c57526b1ec3bf592706c
Alexei <aelinkov@gmail.com>
2023-06-14 10:36:45 +0300

    deploy to cyclic

--- src/app.ts
+++ src/app.ts
@@ -63,7 +63,7 @@ import { DbService } from "./db/db.service";
 //     return false;
 //   });
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3003;
 app.get("/", (req: Request, res: Response) => res.send("Hello World!!!"));
 app.listen(port, () => {
   console.log(`Bot app listening on port ${port}!`);
